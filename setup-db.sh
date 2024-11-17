d54561fa5ed135b6052e48d546cc329c9429c848
platypus <platypus.computerchip@gmail.com>
2024-11-17 16:05:11 +0900

    Improve setup-db.sh

--- setup-db.sh
+++ setup-db.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
-python3 manage.py migrate
-python3 manage.py loaddata navbar
-python3 manage.py loaddata language_small
-python3 manage.py loaddata demo
-python3 manage.py addjudge $JUDGE_NAME $JUDGE_KEY || true
+tables=$(echo "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'dmoj'" | python3 manage.py dbshell -- -N)
+if [ "$tables" -eq 0 ]; then
+    python3 manage.py migrate
+    python3 manage.py loaddata navbar
+    python3 manage.py loaddata language_small
+    python3 manage.py loaddata demo
+    python3 manage.py addjudge $JUDGE_NAME $JUDGE_KEY
+fi
 
 python3 manage.py runserver 0.0.0.0:8000
